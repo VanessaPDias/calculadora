48abf5f9dea4f604007883ccc4ae1f1941492d0e
= <vanevanessa.dias@gmail.com>
2021-05-28 16:38:38 +0100

    correção de bugs

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-let valores = "0";
 window.onload = aoCarregarPagina;
 
-function aoCarregarPagina () {
+function aoCarregarPagina() {
     let numeros = document.querySelectorAll('.btn-num');
 
     for (let i = 0; i < numeros.length; i++) {
@@ -14,86 +13,117 @@ function aoCarregarPagina () {
         operadores[i].onclick = opClicado;
     };
 
-    document.querySelector('.btn-resultado').onclick = calcular;
-    document.querySelector('.btn-porcentagem').onclick = calcularPorcentagem;
+    document.querySelector('#btn-resultado').onclick = calcular;
+    document.querySelector('#btn-porcentagem').onclick = calcularPorcentagem;
     document.querySelector('#limpa-tudo').onclick = limpar;
     document.querySelector('#exclui-digito').onclick = excluirDigito;
     document.querySelector('#inverte-sinal').onclick = inverterSinal;
+    document.querySelector('#btn-ponto').onclick = adicionarPonto;
     document.querySelector('#visor').value = valores;
 };
 
+let valores = "0";
+let calculoFeito = false;
+
 function numClicado(evt) {
-    if(document.querySelector('#visor').value == '0' || document.querySelector('#visor').value != ''){
+    if (document.querySelector('#visor').value == '0') {
         valores = "";
         document.querySelector('#visor').value = "";
         document.querySelector('#retorno-tela').innerHTML = valores;
     };
 
+    if (calculoFeito == true) {
+        document.querySelector('#visor').value = "";
+        document.querySelector('#retorno-tela').innerHTML = "";
+        valores = "";
+        calculoFeito = false;
+    };
+    console.log(4, valores)
     let valorElemento = evt.target.innerHTML;
     let valorRetornado = document.querySelector('#visor').value += valorElemento;
-    
-
+    console.log(5, valores)
 };
 
 function opClicado(evt) {
+    console.log(6, valores)
     let numero = document.querySelector('#visor').value;
     let operador = evt.target.innerHTML;
-    
-    if(numero == "") {
+
+    if (numero == "") {
         valores = "";
         return;
     };
-
-
-
+    console.log(7, valores)
     document.querySelector('#visor').value = "";
-
     valores = `${valores} ${numero} ${operador}`;
     document.querySelector('#retorno-tela').innerHTML = valores;
-   
+    console.log(8, valores)
+    calculoFeito = false;
 };
 
 
 function calcular(evt) {
+    calculoFeito = true;
     let numero = document.querySelector('#visor').value;
     let operador = evt.target.innerHTML;
-    
-    valores = `${valores} ${numero}`;
 
+    valores = `${valores} ${numero}`;
     document.querySelector('#retorno-tela').innerHTML = `${valores} ${operador}`;
-    
     document.querySelector('#visor').value = eval(valores);
-    
     valores = "";
-   
 };
 
-function calcularPorcentagem() {
+console.log(0, valores)
+function adicionarPonto() {
     let numero = document.querySelector('#visor').value;
 
     if(numero == '0') {
         valores = "";
+        document.querySelector('#visor').value = `0.`;
+    }else if(numero == ""){
+        document.querySelector('#visor').value = `0.`;
+    }
+    else {
+        document.querySelector('#visor').value = `${numero}.`;
+        
+    }
+
+    calculoFeito = false;
+    console.log(1, numero)
+    console.log(2, valores)
+};
+
+
+
+function calcularPorcentagem() {
+    let numero = document.querySelector('#visor').value;
+
+    if (numero == '0') {
+        document.querySelector('#retorno-tela').innerHTML = valores;
+        valores = "";
         return;
     };
-    
-    valores = `${valores} (${numero} / 100)`;
 
+    valores = `${valores} (${numero} / 100)`;
     document.querySelector('#retorno-tela').innerHTML = valores;
     document.querySelector('#visor').value = eval(valores);
     valores = "";
-}; 
+};
 
-function limpar(){
+
+function limpar() {
     document.querySelector('#visor').value = '0';
     document.querySelector('#retorno-tela').innerHTML = "";
-    valores = "";
+    valores = '0';
 };
+
 
 function excluirDigito() {
     let numero = document.querySelector('#visor').value;
     numero = numero.slice(0, -1);
     document.querySelector('#visor').value = numero;
 };
+
 
 function inverterSinal() {
     let numero = document.querySelector('#visor').value;
